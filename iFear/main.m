3b19e5efcb895f08b3c3bd6210ed7bc58067f282
dacioleal <dacioleal@gmail.com>
2014-01-08 10:35:46 +0100

    Initial Commit

---
+++ iFear/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iFear
+//
+//  Created by Dacio Leal Rodriguez on 08/01/14.
+//  Copyright (c) 2014 Dacio Leal Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
