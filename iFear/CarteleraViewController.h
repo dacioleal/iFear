b2ca3373bbe990416e840eea897442a06ff1449d
dacioleal <dacioleal@gmail.com>
2014-01-20 17:28:50 +0100

    Añadidos iconos en bundle

---
+++ iFear/CarteleraViewController.h
@@ -0,0 +1,17 @@
+//
+//  CarteleraViewController.h
+//  iFear
+//
+//  Created by Dacio Leal Rodriguez on 13/01/14.
+//  Copyright (c) 2014 Dacio Leal Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CarteleraViewController : UIViewController
+
+- (IBAction)volverPushButton:(id)sender;
+- (IBAction)buscarPushButton:(id)sender;
+- (IBAction)menuPushButton:(id)sender;
+
+@end
---
+++ iFear/CarteleraViewController.m
@@ -0,0 +1,48 @@
+//
+//  CarteleraViewController.m
+//  iFear
+//
+//  Created by Dacio Leal Rodriguez on 13/01/14.
+//  Copyright (c) 2014 Dacio Leal Rodriguez. All rights reserved.
+//
+
+#import "CarteleraViewController.h"
+
+@interface CarteleraViewController ()
+
+@end
+
+@implementation CarteleraViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)volverPushButton:(id)sender {
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)buscarPushButton:(id)sender {
+}
+
+- (IBAction)menuPushButton:(id)sender {
+}
+@end
