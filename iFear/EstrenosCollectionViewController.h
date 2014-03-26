4d51430274d083492b0b1cd49fd3fbc505f68004
dacioleal <dacioleal@gmail.com>
2014-03-26 13:21:23 +0100

    Prueba Collection View Controller

---
+++ iFear/EstrenosCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  EstrenosCollectionViewController.h
+//  iFear
+//
+//  Created by Dacio Leal Rodriguez on 26/03/14.
+//  Copyright (c) 2014 Dacio Leal Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EstrenosCollectionViewController : UICollectionViewController
+
+@end
