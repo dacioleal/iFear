213d57f929ce93e94ea1336f293db268d1409272
jamarfal <josealbertomartinfalcon@iMac-de-Jose-Alberto.local>
2014-09-14 17:30:05 +0200

    Añadida Vista de Opiniones en Mentes Oscuras

---
+++ iFear/MentesOscurasOpinionesCellTableViewCell.h
@@ -0,0 +1,22 @@
+//
+//  MentesOscurasOpinionesCellTableViewCell.h
+//  iFear
+//
+//  Created by José Alberto Martín Falcón on 14/09/14.
+//  Copyright (c) 2014 Dacio Leal Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MentesOscurasOpinionesCellTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *lblMovieTitle;
+@property (weak, nonatomic) IBOutlet UILabel *lblFlash;
+@property (weak, nonatomic) IBOutlet UILabel *lblFlashReview;
+@property (weak, nonatomic) IBOutlet UILabel *lblReviewUser;
+@property (weak, nonatomic) IBOutlet UILabel *lblReviewTitle;
+@property (weak, nonatomic) IBOutlet UILabel *lblTerror;
+@property (weak, nonatomic) IBOutlet UILabel *lblGore;
+@property (weak, nonatomic) IBOutlet UILabel *lblHumor;
+@property (weak, nonatomic) IBOutlet UILabel *lblCalidad;
+
+@end
